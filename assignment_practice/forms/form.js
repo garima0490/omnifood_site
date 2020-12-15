81ea98b80e72ee3be11add29a0d54da325cbffd6
Garima Sharma <garima4998@gmail.com>
2020-12-16 00:06:23 +0530

    adding form style and displaying form data on submit

--- assignment_practice/forms/form.js
+++ assignment_practice/forms/form.js
@@ -1,27 +1,35 @@
-function validateForm() {
+function displayAfterValidate() {
+
+    // Validating Name 
   if( document.signupForm.name.value == "" ) {
     alert( "Name field cannot be blank!" );
     document.signupForm.name.focus() ;
     return false;
   }   
   
+  // Validating Phone 
   if( document.signupForm.phone.value.length !=10 || isNaN(document.signupForm.phone.value) ) {
     alert( "Phone field should contain exactly 10 digits!" );
     document.signupForm.phone.focus() ;
     return false;
   }   
 
+  // Validating Email
   if(!validateEmail())
     return false;
 
-  if( document.signupForm.source.value == "-1" ) {
-    alert( "Please select one option!" );
-    document.signupForm.source.focus() ;
-    return false;
- }
-
+    // Displaying the form data
+  var name=document.signupForm.name.value;
+  var email=document.signupForm.email.value;
+  var phone=document.signupForm.phone.value;
+  
+  document.write(`<h1>Thanks for signing up ${name.fontcolor("blue")}</h1><br><h2>Following is the information associated with your account : </h2> <br>`);
+  document.write(`<h3>Phone :  ${phone.fontcolor("blue")}<br> Email : ${email.fontcolor("blue")}</h3>`); 
+  
+  return true;
 } 
 
+// Function for validating Email
 function validateEmail() {
   var email= document.signupForm.email.value;
   atpos = email.indexOf("@");
@@ -32,5 +40,8 @@ function validateEmail() {
     document.signupForm.email.focus() ;
     return false;
   }
-  return( true );
+  return true;
  }
+
+
+  
